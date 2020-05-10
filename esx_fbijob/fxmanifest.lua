d180de5a9f95754b03520776480b363e1778b22c
ShaggyRogersTBS <62436188+ShaggyRogersTBS@users.noreply.github.com>
2020-05-11 02:15:23 +0300

    Update fxmanifest.lua

--- esx_fbijob/fxmanifest.lua
+++ esx_fbijob/fxmanifest.lua
@@ -2,39 +2,23 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'ESX Police Job'
+description 'ESX FBI Job'
 
-version '1.3.0'
+version '1.0.0'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
+	'locales/bg.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/ko.lua',
-	'locales/cs.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
+	'locales/bg.lua',
 	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/ko.lua',
-	'locales/cs.lua',
 	'config.lua',
 	'client/main.lua',
 	'client/vehicle.lua'
